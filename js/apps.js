a1410b3da93d34a1769b63f0b59c5909c50e3ee7
rizky_feb <rizkydurfeb976@gmail.com>
2024-04-30 03:09:43 +0700

    Done login register

--- js/apps.js
+++ js/apps.js
@@ -1,20 +1,27 @@
-const formOpenBtn = document.querySelector("#form-open"),
-  home = document.querySelector(".home"),
-  formContainer = document.querySelector(".form-container"),
-  formCloseBtn = document.querySelector(".form_close"),
+const formContainer = document.querySelector(".form-container"),
   signupBtn = document.querySelector("#signup"),
   loginBtn = document.querySelector("#login"),
   pwShowHide = document.querySelectorAll(".pw_hide");
 
-formOpenBtn.addEventListener("click", () => home.classList.add("show"));
-formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
-
-signupBtn.addEventListener("cilick", (e) => {
-    e.preventDefault();
-    formContainer.classList.add("active");
+pwShowHide.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let getPwInput = icon.parentElement.querySelector("input");
+    if (getPwInput.type === "password") {
+      getPwInput.type = "text";
+      icon.classList.replace("uil-eye-slash", "uil-eye");
+    } else {
+      getPwInput.type = "password";
+      icon.classList.replace("uil-eye", "uil-eye-slash");
+    }
+  });
 });
 
-loginBtn.addEventListener("cilick", (e) => {
-    e.preventDefault();
-    formContainer.classList.remove("active");
+signupBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.add("active");
+});
+
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.remove("active");
 });
